b33bbf0f04ef22be9582e2fe45153b59f9c51ef5
BTurkozen <BkTurkozen@gmail.com>
2020-11-18 12:13:34 +0300

    JWT oluşturldu.

---
+++ Restaurant-Angular.Common/Helpers/JwtTokenPacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Angular.Common.Helpers
+{
+    public class JwtTokenPacket
+    {
+        public string Token { get; set; }
+        public string Expiretion { get; set; }
+        public string UserName { get; set; }
+    }
+}
