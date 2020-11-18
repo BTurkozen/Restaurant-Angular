6203c69110ed05f0c9401c3524d3758c91ae3689
BTurkozen <BkTurkozen@gmail.com>
2020-11-18 16:04:10 +0300

    Angular Projesi, Component, Model, Service'ler oluşturuldu.

---
+++ Frontend-Project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    OrderComponent,
+    OrderItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
